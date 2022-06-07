9cde5763fb128f6880e3ddb243ea34202971f468
nicolasandrada <nicolas.andrada@hotmail.com>
2022-06-07 18:07:34 -0300

    Initial commit

---
+++ formulario.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/miestilo.css">
+</head>
+<body class="">
+    <form action="" enctype="multipart/form-data">
+        <label for="">Nombre del Organo</label>
+        <input class='form-control' type="text" name="" id="">
+        <label for="">Tipo de Sangre</label>
+        <select class='form-select' name="" id="" >
+            <option value="A+">A+</option>
+            <option value="A-">A-</option>
+            <option value="AB+">AB+</option>
+            <option value="AB-">AB-</option>
+            <option value="O+">O+</option>
+        </select>
+        <label for="">Precio</label>
+        <input class='form-control' type="number" name="" id="" step="0.01">
+        <label for="">Foto del Organo</label>
+        <input class='form-control' type="file" name="" id="">
+        <input class="btn btn-primary my-4" type="submit" value="Guardar">
+    </form>
+</body>
+</html>
+<!--
+    UwUr
+-->
